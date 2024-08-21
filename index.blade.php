4871daa99ef4f90ecc76461f60300ec2bca1560f
chandale255 <josephchandale97@gmail.com>
2024-08-21 10:12:43 +0300

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form>
+        <h1>simple login form</h1>
+        <lable for= "name"> name</label>
+        <input type="name" id="name" name="name"value=""><br><br>
+
+        <lable for="Email">Email</lable>
+        <input type="text" id="email" name="email" value=""><br><br>
+
+        <label for="Password">password</lable>
+        <input type="text"id="password"name="password"value=""><br><br>
+        <button>submit</button>
+        </form>
+    
+</body>
+</html>
